1c3cfc94a27ad5e6a67d25791df828e8262940d5
quevon <quevonmontojo@gmail.com>
2021-11-10 12:55:53 +0800

    first

---
+++ config/piecesImages.config.js
@@ -0,0 +1,15 @@
+
+export const piecesImages = {
+    'white_pawn': './img/white-poon.png',
+    'white_rook': './img/rook-white.png',
+    'white_knight': './img/horse-white.png',
+    'white_bishop': './img/bishop-white.png',
+    'white_king': './img/king-white.png',
+    'white_queen': './img/queen-white.png',
+    'black_pawn': './img/black-poon.png',
+    'black_rook': './img/rook-black.png',
+    'black_knight': './img/horse-black1.png',
+    'black_bishop': './img/bishop-black.png',
+    'black_king': './img/king-black.png',
+    'black_queen': './img/queen-black.png',
+}
